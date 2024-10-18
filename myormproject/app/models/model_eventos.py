45dcae159e8257f839c5e56cd4fcf98b5c5d56f6
gamesferabr <gabriel@timepix.com.br>
2024-10-18 06:14:38 -0700

    novo3

--- myormproject/app/models/model_eventos.py
+++ myormproject/app/models/model_eventos.py
@@ -1,12 +1,19 @@
 from django.db import models
 
 class Evento(models.Model):
+    id = models.CharField(primary_key=True, 
+                          unique=True, 
+                          editable=False,
+                          max_length=50,)
+    
     nome = models.CharField(max_length=500)
-    data_inicio = models.DateTimeField()
-    data_fim = models.DateTimeField()
-    valor = models.FloatField()
+    data_inicio = models.CharField(max_length=500)
+    data_fim = models.CharField(max_length=500)
+    valor = models.CharField(max_length=500)
     local = models.CharField(max_length=500)
     descricao = models.TextField()
-    link_compra = models.URLField()
-    link_validacao = models.URLField()
-    
+    link_compra = models.URLField(default="")
+    link_validacao = models.URLField(default="")
+    url_imagem = models.URLField(default="")
+
+    is_deleted = models.BooleanField(default=False)
